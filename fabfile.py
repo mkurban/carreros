eb880148a0e00c57824978b1836539e4d10e599e
Martin Gaitan <mgaitan@onapsis.com>
2017-08-01 19:45:40 -0300

    req en deploy

--- fabfile.py
+++ fabfile.py
@@ -2,12 +2,13 @@ from fabric.api import run, env, cd
 from carreros.local_settings import HOST_IP, HOST_USER
 
 env.hosts = [HOST_IP]
-env.user = [HOST_USER]
+env.user = HOST_USER
 
 def deploy():
     run("source /virtualenvs/carreros/bin/activate")
     with cd('/projects/carreros'):
         run("git pull")
+        run("/virtualenvs/carreros/bin/pip install -r requirements.txt")
         run("/virtualenvs/carreros/bin/python manage.py migrate")
         run("/virtualenvs/carreros/bin/python manage.py collectstatic --noinput")
         run("supervisorctl restart carreros")
