f51276e8f6cbc8b16b3850d537775f65c1f285a5
Martin Gaitan <mgaitan@onapsis.com>
2017-08-01 19:13:05 -0300

    load popup content via ajax

--- elecciones/urls.py
+++ elecciones/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    url('^escuelas.geojson$', views.LugaresVotacionGeoJSON.as_view(), name="geojson"),
+    url('^escuelas.geojson$', views.LugaresVotacionGeoJSON.as_view(), name='geojson'),
+    url('^escuelas/(?P<pk>\d+)$', views.EscuelaDetailView.as_view(), name='detalle_escuela'),
     url('^mapa$', views.Mapa.as_view(), name='mapa'),
     url('^mapa1$', views.Mapa1.as_view(), name='mapa1')
+
 ]
--- elecciones/views.py
+++ elecciones/views.py
@@ -1,15 +1,21 @@
 from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
 from djgeojson.views import GeoJSONLayerView
 from .models import LugarVotacion
 
 
 class LugaresVotacionGeoJSON(GeoJSONLayerView):
     model = LugarVotacion
-    properties = ('popup_html',)
+    properties = ('id',) # 'popup_html',)
 
     def get_queryset(self):
         qs = super().get_queryset()
         return qs.filter(latitud__isnull=False)
+
+
+class EscuelaDetailView(DetailView):
+    template_name = "elecciones/detalle_escuela.html"
+    model = LugarVotacion
 
 
 # Create your views here.
