be6278c60068869badcbf81cded46cfc21496ae6
Martin Gaitan <mgaitan@onapsis.com>
2017-08-05 19:10:30 -0300

    card default si todavia no tiene asignaciones

--- fabfile.py
+++ fabfile.py
@@ -5,10 +5,14 @@ from carreros.local_settings import HOST_IP, HOST_USER
 env.hosts = [HOST_IP]
 env.user = HOST_USER
 
-def shell_plus():
+
+def manage(command):
     run("source /virtualenvs/carreros/bin/activate")
     with cd('/projects/carreros'):
-        run("/virtualenvs/carreros/bin/python manage.py shell_plus")
+        run(f"/virtualenvs/carreros/bin/python manage.py {command}")
+
+def shell_plus():
+    manage('shell_plus')
 
 
 def append_to_local_settings(path):
